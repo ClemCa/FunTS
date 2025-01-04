05ac2425968653fa2612c5732ce0a66ce6421527
ClemCa <perso.catorc@gmail.com>
2025-01-04 16:58:41 +0000

    0.1.3: Multi batching path fix

--- lib/batching.ts
+++ lib/batching.ts
@@ -6,10 +6,12 @@ export function HandleBatch(allPipelines: Map<string, pipeline>, req: any, res: 
         res.status(400).send("Invalid body");
         return;
     }
+    console.log("batch", req.body);
     const results = req.body.map((body: any) => {
         const path = body[0];
-        const pipelineGroup = allPipelines.get(path);
+        const pipelineGroup = allPipelines.get(path) || allPipelines.get("/" + path) || allPipelines.get(path + "/") || allPipelines.get("/" + path + "/") || allPipelines.get(path.replace(/\/$/, ""));
         if (!pipelineGroup) {
+            console.log("Couldn't find pipeline for", path);
             return [404, "Not found"];
         }
         const res = pipelineGroup.length === 0 || PipelineStep(pipelineGroup, 0, body);
