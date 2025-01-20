681af8862fa00d2a72b14e9be7b952fde45985a2
ClemCa <perso.catorc@gmail.com>
2025-01-20 17:38:13 -0400

    0.1.6: Nested batching support, in view of fun-ts/front implementing auto batching of requests

--- lib/batching.ts
+++ lib/batching.ts
@@ -1,5 +1,6 @@
 import { pipeline } from "./pipeline";
-import { PipelineStep } from "./processing";
+import { ProcessPipeline, ProcessPipelineBatch } from "./processing";
+import { Response } from "express";
 
 export function HandleBatch(allPipelines: Map<string, pipeline>, req: any, res: any) {
     if (!req.body || !Array.isArray(req.body) || req.body.some((x: any) => !Array.isArray(x) || typeof x[0] !== "string")) {
@@ -9,18 +10,44 @@ export function HandleBatch(allPipelines: Map<string, pipeline>, req: any, res: 
     console.log("batch", req.body);
     const results = req.body.map((body: any) => {
         const path = body[0];
+        if(path === "/batch/" || path === "/batch") {
+            let resValue = [400, "Internal error in nested batch"];
+            const myRes = {
+                status: (code: number) => ({
+                    send: (value: any) => {
+                        resValue = [code, value];
+                    }
+                })
+            };
+            HandleBatch(allPipelines, body[1], myRes);
+            return resValue;
+        }
         const pipelineGroup = allPipelines.get(path) || allPipelines.get("/" + path) || allPipelines.get(path + "/") || allPipelines.get("/" + path + "/") || allPipelines.get(path.replace(/\/$/, ""));
         if (!pipelineGroup) {
             console.log("Couldn't find pipeline for", path);
             return [404, "Not found"];
         }
-        for(const p of pipelineGroup) {
-            const result = pipelineGroup.length === 0 || PipelineStep(p, 0, body[1]);
-            if (result) {
-                return result;
+        const singleBatched = body[2];
+        let resValue;
+        const myRes = {
+            status: (code: number) => ({
+                send: (value: any) => {
+                    resValue = [code, value];
+                }
+            })
+        } as Response;
+        if(singleBatched) {
+            resValue = [400, "Internal error in nested batch"];
+            ProcessPipelineBatch(pipelineGroup, req, myRes)
+            return resValue;
+        }
+        resValue = [404, "Not found"];
+        for (const p of pipelineGroup) {
+            if(ProcessPipeline(p, req, myRes)) {
+                return resValue;
             }
         }
-        return [404, "Not found"];
+        return resValue;
     });
     res.status(200).send(results); // no need for teapot, as we never expect the same status code
 }
